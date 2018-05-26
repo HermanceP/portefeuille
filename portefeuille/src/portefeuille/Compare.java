751dd687771615d8b25afc294d8187f4ba123780
tvng <37909639+tvng@users.noreply.github.com>
2018-05-27 00:11:54 +0200

    compare
    j'ai rien compris tho

---
+++ portefeuille/src/portefeuille/Compare.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package portefeuille;
+
+import java.lang.Comparable;
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * J 'AI RIEN COMPRIS A CELUI LA
+ * 
+ * 
+ * 
+ * 
+ * 
+ * @author Tao Tuong Vi
+ */
+public class Compare implements Comparable<Fonds>{
+
+    //on redefinit equals de Object
+    public boolean equals(Fonds f)
+    {
+        return false;
+        
+    }
+    
+    @Override
+    public int compareTo(Fonds f) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
